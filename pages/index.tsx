23fa4568a7f5e30f3fab5e81a1c303c4ea34e1a2
jakubmroz13 <71970935+jakubmroz13@users.noreply.github.com>
2022-04-03 18:06:30 +0200

    Adding single player option

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import { useState } from 'react'
 import Message from '../components/Message'
 import dictionary from '../functions/dictionary'
 import { encrypt } from '../functions/cypher'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+  const router = useRouter()
   const [word, setWord] = useState('')
   const [message, setMessage] = useState('')
   const [linkCopied, setLinkCopied] = useState(false)
@@ -14,7 +16,7 @@ const Home: NextPage = () => {
     if (word) {
       if (word.length === 5) {
         if (dictionary.has(word.toLowerCase())) {
-          navigator.clipboard.writeText('localhost:3000/' + encrypt(word.toLowerCase()))
+          navigator.clipboard.writeText('https://literalniebezlimitu.fun/' + encrypt(word.toLowerCase()))
           setMessage('')
           setLinkCopied(true)
         } else {
@@ -25,7 +27,14 @@ const Home: NextPage = () => {
       }
     } else {
       setMessage('Podaj swoje słowo')
-    } 
+    }
+  }
+
+  const handleRandomWord = () => {
+    const arr = Array.from(dictionary)
+    const randomWord = arr[Math.round(Math.random() * arr.length)]
+    const randomURL = encrypt(randomWord)
+    router.push('/' + randomURL)
   }
 
   return (
@@ -43,7 +52,11 @@ const Home: NextPage = () => {
           setWord(e.target.value)
           setLinkCopied(false)
         }} placeholder="Wpisz swoje słowo" className="m-1 h-12 rounded text-center text-neutral-500 outline-none"></input>
-        <button onClick={handleCopyURL} className="m-1 h-12 bg-neutral-400 rounded hover:bg-green-500 focus:bg-green-500">{linkCopied ? 'Link skopiowany' : 'Skopiuj link'}</button>
+        <button onClick={handleCopyURL} className="m-1 h-12  bg-neutral-400 rounded hover:bg-green-500 focus:bg-green-500">{linkCopied ? 'Link skopiowany' : 'Skopiuj link'}</button>
+      </div>
+
+      <div className="flex flex-col w-4/5 max-w-md text-xl m-auto mt-20">
+        <button onClick={handleRandomWord} className="m-1 h-12  bg-neutral-400 rounded hover:bg-green-500 focus:bg-green-500">Losuj słowo dla Siebie</button>
       </div>
     </div>
   )
